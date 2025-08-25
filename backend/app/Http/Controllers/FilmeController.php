8ea0b517dbb5ee84c51c9e8202c3eb214f3a876d
Luis Modesto <luisgmodesto12@gmail.com>
2025-08-25 03:52:16 -0300

    feat: create filme controller

---
+++ backend/app/Http/Controllers/FilmeController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Filme;
+
+class FilmeController
+{
+ 
+    public function index()
+    {
+        $filmes = Filme::paginate(10);
+        return response()->json($filmes, 200);
+    }
+
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'titulo' => 'required|string',
+            'ano' => 'required|integer',
+            'genero' => 'required|string',
+            'sinopse' => 'nullable|string',
+            'poster_url' => 'nullable|string'
+        ]);
+
+        $filme = Filme::create($validated);
+
+        return response()->json($filme, 201);
+    }
+
+
+    public function show(string $id)
+    {
+       $filme = Filme::findOrFail($id);
+
+       if(!$filme) {
+        return response()->json(['message' => 'Filme não encontrado'], 404);
+       }
+
+       return response()->json($filme, 200);
+    }
+
+
+    public function update(Request $request, string $id)
+    {
+        $filme = Filme::find($id);
+
+        if (!$filme) {
+            return response()->json(['message' => 'Filme não encontrado'], 404);
+        }
+
+        $validated = $request->validate([
+            'titulo' => 'sometimes|required|string',
+            'ano' => 'sometimes|required|string',
+            'genero' => 'sometimes|required|string',
+            'sinopse' => 'nullable|string',
+            'poster_url' => 'nullable|string'
+        ]);
+
+        $filme->update($validated);
+
+        return response()->json($filme, 200);
+    }
+
+    public function destroy(string $id)
+    {
+        $filme = Filme::find($id);
+
+        if (!$filme){
+            return response()->json(['message' => 'Filme não encontrado'], 404);
+        }
+
+        $filme->delete();
+
+        return response()->json(['message' => "Filme deletado com sucesso", 200]);
+    }
+}
