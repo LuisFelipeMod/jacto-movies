8ba5e490daa81fea25eceebdd3aa2994b0b3913c
Luis Modesto <luisgmodesto12@gmail.com>
2025-08-25 03:52:07 -0300

    feat: create filme model

---
+++ backend/app/Models/Filme.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Filme extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'titulo',
+        'ano',
+        'genero',
+        'sinopse',
+        'poster_url'
+    ];
+}
