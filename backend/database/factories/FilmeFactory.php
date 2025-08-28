94081931efb12341ff08efd9667d580b097693ae
Luis Modesto <luisgmodesto12@gmail.com>
2025-08-28 04:06:52 -0300

    refactor: set fix url in poster_url factory

--- backend/database/factories/FilmeFactory.php
+++ backend/database/factories/FilmeFactory.php
@@ -18,7 +18,7 @@ class FilmeFactory extends Factory
             'ano' => $this->faker->numberBetween(2000, 2025),
             'genero' => $this->faker->randomElement(['Ação','Comédia','Drama','Suspense','Ficção']),
             'sinopse' => $this->faker->paragraph(),
-            'poster_url' => $this->faker->imageUrl(200, 300, 'movies', true),
+            'poster_url' => "https://placehold.co/600x400?text=Filme",
         ];
     }
 }
